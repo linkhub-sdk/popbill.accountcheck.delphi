4d50033ac2a8ed8ee6f4a39dcc84726934e4feba
ProWON <pro_won45@naver.com>
2022-01-07 15:27:46 +0900

    added ChekcDepositorInfo of AccountCheckService

--- PopbillAccountCheck.pas
+++ PopbillAccountCheck.pas
@@ -20,17 +20,32 @@ type
                 accountName : string;
                 checkDate : Double;
                 resultCode : string;
+                result : string;
+                resultMessage : string;
+        end;
+
+        TDepositorCheckInfo = class
+        public
+                bankCode : string;
+                accountNumber : string;
+                accountName : string;
+                checkDate : Double;
+                identityNumType : string;
+                identityNum : string;
+                result : string;
                 resultMessage : string;
         end;
 
         TAccountCheckService = class(TPopbillBaseService)
         private
                 function jsonToTAccountCheckInfo(json : String) : TAccountCheckInfo;
+                function jsonToTDepositorCheckInfo(json : String) : TDepositorCheckInfo;
 
         public
                 constructor Create(LinkID : String; SecretKey : String);
                 function GetUnitCost(CorpNum : String): Single;
                 function CheckAccountInfo(CorpNum : String; BankCode : String; AccountNumber : String; UserID : String = '') : TAccountCheckInfo;
+                function CheckDepositorInfo(CorpNum : String; BankCode : String; AccountNumber : String; IdentityNumType : String; IdentityNum : String; UserID : String = '') : TDepositorCheckInfo;
                 function GetChargeInfo(CorpNum : String) : TAccountCheckChargeInfo;
         end;
 
@@ -41,6 +56,7 @@ constructor TAccountCheckService.Create(LinkID : String; SecretKey : String);
 begin
        inherited Create(LinkID,SecretKey);
        AddScope('182');
+       AddScope('183');
 end;
 
 function TAccountCheckService.GetUnitCost(CorpNum : String) : Single;
@@ -140,6 +156,11 @@ begin
                 result.resultCode := getJsonString(json, 'resultCode');
         end;
 
+        if Length(getJsonString(json, 'result')) > 0 then
+        begin
+                result.result := getJsonString(json, 'result');
+        end;
+
         if Length(getJsonString(json, 'resultMessage')) > 0  then
         begin
                 result.resultMessage := getJsonString(json, 'resultMessage');
@@ -172,8 +193,6 @@ var
         responseJson : string;
 begin
 
-
-        
         try
                 responseJson := httppost('/EasyFin/AccountCheck?c='+BankCode+'&&n='+AccountNumber, CorpNum, UserID, '', '');
         except
@@ -199,6 +218,86 @@ begin
         else
         begin
                 result := jsonToTAccountCheckInfo(responseJson);
+        end;
+
+end;
+
+function TAccountCheckService.jsonToTDepositorCheckInfo(json : String) : TDepositorCheckInfo;
+begin
+        result := TDepositorCheckInfo.Create;
+
+        if Length(getJsonString(json, 'result')) > 0 then
+        begin
+                result.result := getJsonString(json, 'result');
+        end;
+
+        if Length(getJsonString(json, 'resultMessage')) > 0  then
+        begin
+                result.resultMessage := getJsonString(json, 'resultMessage');
+        end;
+
+        if Length(getJsonString(json, 'bankCode')) > 0 then
+        begin
+                result.bankCode := getJsonString(json, 'bankCode');
+        end;
+
+        if Length(getJsonString(json, 'accountNumber')) > 0  then
+        begin
+               result.accountNumber := getJsonString(json, 'accountNumber');
+        end;
+
+        if Length(getJsonString(json, 'accountName')) > 0 then
+        begin
+              result.accountName := getJsonString(json, 'accountName');
+        end;
+
+        if Length(getJsonString(json, 'checkDate')) > 0 then
+        begin
+              result.checkDate := getJSonFloat(json, 'checkDate');
+        end;
+
+        if Length(getJsonString(json, 'identityNumType')) > 0 then
+        begin
+              result.identityNumType := getJsonString(json, 'identityNumType');
+        end;
+
+        if Length(getJsonString(json, 'identityNum')) > 0 then
+        begin
+              result.identityNum := getJsonString(json, 'identityNum');
+        end;
+end;
+
+function TAccountCheckService.CheckDepositorInfo(CorpNum : String; BankCode : String; AccountNumber : String; IdentityNumType : String; IdentityNum : String; UserID : String = '') : TDepositorCheckInfo;
+var
+        uri : string;
+        responseJson : string;
+begin
+        try
+                uri := '/EasyFin/DepositorCheck?c='+BankCode+'&&n='+AccountNumber+'&&t='+IdentityNumType+'&&p='+IdentityNum;
+                responseJson := httppost(uri, CorpNum, UserID, '', '');
+        except
+                on le : EPopbillException do begin
+                        if FIsThrowException then
+                        begin
+                                raise EPopbillException.Create(le.code,le.Message);
+                                exit;
+                        end
+                        else
+                        begin
+                                result := TDepositorCheckInfo.Create;
+                                exit;
+                        end;
+                end;
+        end;
+        
+        if LastErrCode <> 0 then
+        begin
+                result := TDepositorCheckInfo.Create;
+                exit;
+        end
+        else
+        begin
+                result := jsonToTDepositorCheckInfo(responseJson);
         end;
 
 end;
